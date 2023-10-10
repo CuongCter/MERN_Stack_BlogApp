674579279cfea039cc7262057bfcc54a8b8b2559
CuongCter <cuongcter442002@gmail.com>
2023-10-10 16:21:38 +0700

    :seedling: feat: Article card component

--- fontend/src/pages/home/HomePage.jsx
+++ fontend/src/pages/home/HomePage.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import MainLayout from '../../components/MainLayout'
 import Hero from './container/Hero'
+import Articles from "./container/Articles";
 
 const HomePage = () => {
   return (
     <div>
-        <MainLayout>
-            <Hero> </Hero>
-        </MainLayout>
+      <MainLayout>
+        <Hero />
+        <Articles />
+      </MainLayout>
     </div>
   )
 }
