7d6a9a8415e39efb0fe0b732845afcc529591eef
CuongCter <cuongcter442002@gmail.com>
2023-10-20 00:43:46 +0700

    :pencil2: feat: get user profile data

--- frontend/src/services/index/users.js
+++ frontend/src/services/index/users.js
@@ -17,10 +17,27 @@ export const signup = async ({ name, email, password }) => {
 
 export const login = async ({ email, password }) => {
   try {
-    const { data } = await axios.post("/api/users/login", {
+    const { data } = await axios.post("http://localhost:5000/api/users/login", {
       email,
       password,
     });
+    return data;
+  } catch (error) {
+    if (error.response && error.response.data.message)
+      throw new Error(error.response.data.message);
+    throw new Error(error.message);
+  }
+};
+
+export const getUserProfile = async ({ token }) => {
+  try {
+    const config = {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    };
+
+    const { data } = await axios.get("http://localhost:5000/api/users/profile", config);
     return data;
   } catch (error) {
     if (error.response && error.response.data.message)
