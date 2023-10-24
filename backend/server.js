72e2011c6d0ad67ac40c54bfe0e8cb606aa45662
CuongCter <cuongcter442002@gmail.com>
2023-10-24 16:19:47 +0700

    :sparkles: feat: Create & update post endpoint

--- backend/server.js
+++ backend/server.js
@@ -10,6 +10,8 @@ import {
 
 // Routes
 import userRoutes from "./routes/userRoutes";
+import postRoutes from "./routes/postRoutes";
+
 dotenv.config();
 connectDB();
 const app = express();
@@ -20,6 +22,7 @@ app.get("/", (req, res) => {
 });
 
 app.use("/api/users", userRoutes);
+app.use("/api/posts", postRoutes);
 
 // static assets
 app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
