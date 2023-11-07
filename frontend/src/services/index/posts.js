9e9aea60c8bcd58af258fb344b24a9914d609a3b
CuongCter <cuongcter442002@gmail.com>
2023-11-07 14:16:48 +0700

    :rocket: feat: Fetch single post data

--- frontend/src/services/index/posts.js
+++ frontend/src/services/index/posts.js
@@ -1,8 +1,19 @@
 import axios from "axios";
-
+import { API } from "../../constants/const.api";
 export const getAllPosts = async () => {
   try {
-    const { data } = await axios.get("/api/posts");
+    const { data } = await axios.get(`${API}/api/posts`);
+    return data;
+  } catch (error) {
+    if (error.response && error.response.data.message)
+      throw new Error(error.response.data.message);
+    throw new Error(error.message);
+  }
+};
+
+export const getSinglePost = async ({ slug }) => {
+  try {
+    const { data } = await axios.get(`/api/posts/${slug}`);
     return data;
   } catch (error) {
     if (error.response && error.response.data.message)
