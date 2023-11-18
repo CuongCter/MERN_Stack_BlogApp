77f58dac8183b3cbad0233bc94d4447b2d698cbd
CuongCter <cuongcter442002@gmail.com>
2023-11-18 14:34:38 +0700

    :lipstick: feat: FE Search between different posts and pagination component

--- frontend/src/services/index/posts.js
+++ frontend/src/services/index/posts.js
@@ -1,9 +1,11 @@
 import axios from "axios";
 import { API } from "../../constants/const.api";
-export const getAllPosts = async () => {
+export const getAllPosts = async (searchKeyword = "", page = 1, limit = 10) => {
   try {
-    const { data } = await axios.get(`${API}/api/posts`);
-    return data;
+    const { data, headers } = await axios.get(
+      `${API}/api/posts?searchKeyword=${searchKeyword}&page=${page}&limit=${limit}`
+    );
+    return { data, headers }; 
   } catch (error) {
     if (error.response && error.response.data.message)
       throw new Error(error.response.data.message);
