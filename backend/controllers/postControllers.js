cd079e2aae2999ae39e999bc037165a58ef35e3a
CuongCter <cuongcter442002@gmail.com>
2023-11-20 11:05:45 +0700

    feat: BE delete a post

--- backend/controllers/postControllers.js
+++ backend/controllers/postControllers.js
@@ -155,12 +155,20 @@ const getAllPosts = async (req, res, next) => {
     const page = parseInt(req.query.page) || 1;
     const pageSize = parseInt(req.query.limit) || 10;
     const skip = (page - 1) * pageSize;
-    const total = await Post.countDocuments();
+    const total = await Post.find(where).countDocuments();
     const pages = Math.ceil(total / pageSize);
 
+
+    res.header({
+      "x-filter": filter,
+      "x-totalcount": JSON.stringify(total),
+      "x-currentpage": JSON.stringify(page),
+      "x-pagesize": JSON.stringify(pageSize),
+      "x-totalpagecount": JSON.stringify(pages),
+    });
+
     if (page > pages) {
-      const error = new Error("No page found");
-      return next(error);
+      return res.json([]);
     }
 
     const result = await query
@@ -173,14 +181,6 @@ const getAllPosts = async (req, res, next) => {
         },
       ])
       .sort({ updatedAt: "desc" });
-
-    res.header({
-      "x-filter": filter,
-      "x-totalcount": JSON.stringify(total),
-      "x-currentpage": JSON.stringify(page),
-      "x-pagesize": JSON.stringify(pageSize),
-      "x-totalpagecount": JSON.stringify(pages),
-    });
 
     return res.json(result);
   } catch (error) {
