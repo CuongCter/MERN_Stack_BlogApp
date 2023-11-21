377a4ce36ffd1e3c26d87e1c3126fc5a8f75a320
CuongCter <cuongcter442002@gmail.com>
2023-11-21 12:10:43 +0700

    :lipstick: FE adding image to the content of a post

--- frontend/src/components/editor/Editor.jsx
+++ frontend/src/components/editor/Editor.jsx
@@ -1,4 +1,3 @@
-
 import { EditorContent, useEditor } from "@tiptap/react";
 import "highlight.js/styles/atom-one-dark.css";
 import MenuBar from "./MenuBar";
@@ -11,7 +10,7 @@ const Editor = ({ onDataChange, content, editable }) => {
     editorProps: {
       attributes: {
         class:
-          "prose dark:prose-invert prose-sm sm:prose-base max-w-none mt-7 focus:outline-none prose-pre:bg-[#282c34] prose-pre:text-[#abb2bf]",
+          "!prose !dark:prose-invert prose-sm sm:prose-base max-w-none mt-7 focus:outline-none prose-pre:bg-[#282c34] prose-pre:text-[#abb2bf]",
       },
     },
     onUpdate: ({ editor }) => {
